04bf5e3b6bf2d705b7f7c5d61b7db275997d431a
Joseph Ligier <joseph.ligier@ymagis.com>
2019-01-15 17:23:41 +0100

    Add addition of bands

--- huawei-lte-band/login.py
+++ huawei-lte-band/login.py
@@ -13,6 +13,46 @@ login = 'admin'
 default_ip = '192.168.1.1'
 test_design = True
 
+bands_list = [
+    'b1',
+    'b2',
+    'b3',
+    'b4',
+    'b5',
+    'b6',
+    'b7',
+    'b8',
+    'b19',
+    'b20',
+    'b26',
+    'b28',
+    'b32',
+    'b38',
+    'b40',
+    'b41',
+]
+
+def get_upload_bands(ids):
+    res_bands_list = []
+    for band in bands_list:
+        if ids[band + '_ul'].active:
+            res_bands_list.append(band)
+    return res_bands_list
+
+def get_download_bands(ids):
+    res_bands_list = []
+    for band in bands_list:
+        if ids[band].active:
+            res_bands_list.append(band)
+    return res_bands_list
+
+def convert_list2hex(bands_list):
+    res_int = 0
+    for band in bands_list:
+        power = int(band.replace('b', '')) - 1
+        res_int += 2 ** power
+    return str(hex(res_int)).replace('0x', '')
+
 class LoginPage(Screen):
     def verify_credentials(self):
         ip = self.ids["ip"].text
@@ -35,7 +75,10 @@ class LoginPage(Screen):
 
 class UserPage(Screen):
     def configure_router(self):
-        print(self.ids["b1"].active)
+        down_bands = get_download_bands(self.ids)
+        up_bands = get_upload_bands(self.ids)
+        print(convert_list2hex(down_bands))
+        print(convert_list2hex(up_bands))
 
 class ScreenManagement(ScreenManager):
     pass
