504b13e67abc1c37ab678cd3336f2587e21ac8ea
Joseph Ligier <joseph.ligier@ymagis.com>
2019-01-20 09:31:56 +0100

    Add monitoring view

--- huawei-lte-band/login.py
+++ huawei-lte-band/login.py
@@ -3,18 +3,42 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang.builder import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
 
 from math_bands import *
 from default_value import *
 from huawei_lte_lib import *
+from GraphicSignalInfo import *
 
-class LoginPage(Screen):
+import time
+
+def human_readable_size(size, decimal_places):
+    for unit in ['bps','Kbps','Mbps','Gbps','Tbps']:
+        if size < 1000.0:
+            if unit == 'bps':
+                decimal_places = 0
+            break
+        size /= 1000.0
+    return f"{size:.{decimal_places}f}{unit}"
+
+def convert_bands_list2str(bands_list):
+    res_str = ''
+    for band in bands_list:
+        res_str += bands_ui_dict[band] + '\n'
+    return res_str
+
+
+class LoginPage(BoxLayout):
+    def __init__(self,**kwargs):
+        super(LoginPage,self).__init__(**kwargs)
+        self.monitor_popup = MonitorPage()
+        self.band_popup = BandPage()
+
     def verify_credentials(self):
         ip = self.ids["ip"].text
         password = self.ids["passw"].text
-
-        if password == '':
-            LoginApp().run()
 
         if ip == '':
             ip = default_ip
@@ -23,12 +47,44 @@ class LoginPage(Screen):
 
         if test_design or huawei_lte.check_connection():
             print("Good password")
-            self.manager.current = "user"
-            print(huawei_lte.get_all_monitor_information())
+            self.monitor_popup.open()
+            self.monitor_popup.monitor()
         else:
             print("Bad password")
 
-class BandPage(Screen):
+class MonitorPage(Popup):
+    def __init__(self,**kwargs):
+        super(MonitorPage,self).__init__(**kwargs)
+        self.band_popup = BandPage()
+
+    def display_info(self):
+        print(huawei_lte.get_all_monitor_information())
+
+    def monitor(self, iteration=10):
+        self.iterations = iteration
+        self.score = 0
+        self.event = Clock.schedule_interval(self.set_label, 1)
+
+
+    def set_label(self, dt):
+        self.score += 1
+        info = huawei_lte.get_all_monitor_information()
+        print(info)
+        self.title = "Monitoring %s" %str(self.score)
+        graphic_signal = Graphic_Signal_Info(info['rsrq'], info['rsrp'], info['sinr'])
+
+        self.rsrq.text = graphic_signal.get_rsrq_string()
+        self.rsrp.text = graphic_signal.get_rsrp_string()
+        self.sinr.text = graphic_signal.get_sinr_string()
+        self.upload_rate.text = human_readable_size(info['upload_rate'], 1)
+        self.upload_band.text = bands_ui_dict[info['upload_band']]
+        self.download_rate.text = human_readable_size(info['download_rate'], 1)
+        self.download_band.text = convert_bands_list2str(info['download_band'])
+        if self.score >= self.iterations:
+            self.event.cancel()
+
+class BandPage(Popup):
+
     def configure_router(self):
         down_bands = self.get_bands()
         up_bands = self.get_bands('_ul')
